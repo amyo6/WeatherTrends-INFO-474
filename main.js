0f5cb2bd34c0eca979f74a5c3d4eca84566a26cd
amyo6 <amyo6@uw.edu>
2024-11-22 10:55:33 -0800

    changes

--- main.js
+++ main.js
@@ -111,18 +111,73 @@ d3.csv("weather.csv").then(data => {
     // Edit this in your "index.html" on line 25.
 
     // 6.b: X-axis label
+    svgLine.append("text")
+        .attr("class", "axis-label")
+        .attr("x", width / 2)
+        .attr("y", height + margin.bottom - 10)
+        .attr("text-anchor", "middle")
+        .text("Month");
 
     // 6.c: Y-axis label
+    svgLine.append("text")
+        .attr("class", "axis-label")
+        .attr("transform", "rotate(-90)")
+        .attr("y", -margin.left + 20)
+        .attr("x", -height / 2)
+        .attr("text-anchor", "middle")
+        .text("Avg. Actual Mean Temperature (F)");
 
 
     // 7: ADD LEGEND (if applicable)
     // 7.a: Create legend element
+    svgLine.append("text")
+    .attr("class", "legend-title")
+    .attr("x", width - 90) 
+    .attr("y", -38)         
+    .attr("text-anchor", "start")
+    .style("font-size", "14px")
+    .style("font-weight", "bold")
+    .text("City"); 
+
+    const legend = svgLine.selectAll(".legend")
+        .data(groupedData)
+        .enter()
+        .append("g")
+        .attr("class", "legend")
+        .attr("transform", (d, i) => `translate(${width - 100}, ${i * 20 - 30})`);
 
     // 7.b: Add colored squares
+    legend.append("rect")
+        .attr("x", 10)
+        .attr("width", 10)
+        .attr("height", 10)
+        .style("fill", d => colorScale(d.city)); 
+
 
     // 7.c: Add text
+    legend.append("text")
+        .attr("x", 30)
+        .attr("y", 10)
+        .attr("text-anchor", "start")
+        .style("alignment-baseline", "middle")
+        .text(d => d.city); 
 
 
     // 8: ADD INTERACTIVITY
     // You must have 1-2 interactive elements such as: a tooltip, toggle, dropdown, checklist, number input, time range slider, or zoomable view. With the exception of the tooltip and zoomable view, you must add all interactive elements to "index.html" in the interactive widgets section.
+    // Select the container inside the widget
+    // const checkboxContainer = d3.select("#checkbox-container");
+
+    // // Add checkboxes for each city
+    // checkboxContainer.selectAll("label")
+    //     .data(groupedData)
+    //     .enter()
+    //     .append("label")
+    //     .text(d => d.city)
+    //     .append("input")
+    //     .attr("type", "checkbox")
+    //     .attr("class", "city-checkbox")
+    //     .attr("value", d => d.city)
+    //     .property("checked", true); 
+
 });
